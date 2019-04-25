000cc4d8bd487412a9b42bea46a6697f2bfa0da0
Adrien Driffaud <ad.driffaud@gmail.com>
2019-04-25 10:02:15 +0200

    Use last androidx preference version

--- app/src/main/java/fr/driffaud/band2fit/MainPreferenceFragment.kt
+++ app/src/main/java/fr/driffaud/band2fit/MainPreferenceFragment.kt
@@ -1,15 +1,43 @@
 package fr.driffaud.band2fit
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
+import android.text.method.PasswordTransformationMethod
+
 
 class PreferenceFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-        val preference = findPreference("server_url") as EditTextPreference
-        preference.summary = "Test"
-    }
 
+        val urlEditText = findPreference<EditTextPreference>("server_url")
+        urlEditText?.onBindEditTextListener = EditTextPreference.OnBindEditTextListener { editText ->
+            editText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_URI
+        }
+
+        val userEditText = findPreference<EditTextPreference>("username")
+        userEditText?.onBindEditTextListener = EditTextPreference.OnBindEditTextListener { editText ->
+            editText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+        }
+
+        val passEditText = findPreference<EditTextPreference>("password")
+        passEditText?.onBindEditTextListener = EditTextPreference.OnBindEditTextListener { editText ->
+            editText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            editText.transformationMethod = PasswordTransformationMethod.getInstance()
+        }
+
+        val lastSyncTime = "09:14"
+        val syncSummaryProvider = Preference.SummaryProvider<SwitchPreferenceCompat> { preference ->
+            if (preference.isChecked) {
+                "Last synced at $lastSyncTime"
+            } else {
+                "Syncing is currently disabled"
+            }
+        }
+        findPreference<SwitchPreferenceCompat>("sync_influx")?.summaryProvider = syncSummaryProvider
+    }
 }
